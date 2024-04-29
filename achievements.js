23c3568b9a580a577de91d0c1bc0cf624c79f88e
Faith-yiamat <oloserianyiamat@gmail.com>
2024-04-29 07:38:00 +0300

    add files

---
+++ achievements.js
@@ -0,0 +1,21 @@
+const achievementContainer = document.querySelector(".achievement-container");
+const addButton = document.querySelector(".btn");
+let achievement = document.querySelectorAll(".input-box");
+addButton.addEventListener("click", ()=>{
+let inputBox = document.createElement("p")
+let img = document.createElement("img")
+inputBox.className = "input-box"
+inputBox.setAttribute("contenteditable", "true");
+img.src = "imagefol/image.png"
+achievementContainer.appendChild(inputBox).appendChild(img);
+})
+achievementContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+      e.target.parentElement.remove();
+    }
+})
+
+
+
+
+
